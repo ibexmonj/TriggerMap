82072a0f1eee495528652fe82182b247c38847dc
Manjunath Shenoy <mshenoy@obelix.localdomain>
2025-03-31 20:12:54 -0400

    prod routing changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-const basename = import.meta.env.MODE === 'production' ? '/TriggerMap' : '/';
+const basename = import.meta.env.MODE === 'production' ? '/' : '/';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
